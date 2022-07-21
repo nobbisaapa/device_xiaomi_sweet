4ad501303dc2e3a6460352463b9715ff388825e6
Niranjan BR <niranjankannan2003@gmail.com>
2022-07-21 01:34:56 +0000

    sweet: vendorsetup.sh: change common tree branch
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,7 @@ git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-12.0-c
 git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 
 # Common dt
-git clone https://github.com/Sm6150-Sweet/device_xiaomi_sm6150 device/xiaomi/sm6150-common
+git clone https://github.com/Sm6150-Sweet/device_xiaomi_sm6150 -b spark device/xiaomi/sm6150-common
 
 # Vendor
 git clone https://github.com/Sm6150-Sweet/vendor_xiaomi.git vendor/xiaomi
